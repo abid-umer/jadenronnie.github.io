62046789ba0ddd27fa082f649e23b2557fa22382
jadenronnie <xzabidxz@gmail.com>
2019-05-22 02:33:44 +0500

    Update script.js

--- module-4/script.js
+++ module-4/script.js
@@ -3,15 +3,20 @@ Solution of assignment 4:
 
 Expected output:
 
-Hello Umer
-Good Bye Usman
+Hello Yaakov
 Good Bye John
-Good Bye Alex
+Good Bye Jen
+Good Bye Jason
 Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
 */
 
 (function() {
-    var names = ["Umer", "Usman", "John", "Alex", "Paul"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
